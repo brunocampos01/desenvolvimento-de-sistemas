6a09eb470f84537b841cb22a790239d820151992
Bruno Campos <brunocampos01@gmail.com>
2020-06-26 14:03:42 -0300

    Update ExampleUnitTest.java

--- trabalho-app_android/app/src/test/java/com/example/william/trabalho/ExampleUnitTest.java
+++ trabalho-app_android/app/src/test/java/com/example/william/trabalho/ExampleUnitTest.java
@@ -2,8 +2,6 @@ package com.example.william.trabalho;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
