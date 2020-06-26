bc8821e3ebe2df42e940927eadd394169c57e3bb
Bruno Campos <brunocampos01@gmail.com>
2020-06-26 14:04:43 -0300

    Update ExampleUnitTest.java

--- trabalho-app_android/app/src/test/java/com/example/william/trabalho/ExampleUnitTest.java
+++ trabalho-app_android/app/src/test/java/com/example/william/trabalho/ExampleUnitTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
+    public void additionIsCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
 }
